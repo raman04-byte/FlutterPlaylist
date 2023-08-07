1c8f42a28bc0fbb6ba07234001a2e46c7b463619
raman04-byte <ramantank04022002@gmail.com>
2023-08-07 12:17:37 +0530

    Navigation between Screens

---
+++ navigation/lib/Screens/first_screen.dart
@@ -0,0 +1,32 @@
+import 'package:flutter/material.dart';
+import 'package:navigation/Screens/second_screen.dart';
+
+class FirstScreen extends StatefulWidget {
+  const FirstScreen({super.key});
+
+  @override
+  State<FirstScreen> createState() => _FirstScreenState();
+}
+
+class _FirstScreenState extends State<FirstScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("First Screen"),
+          ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const SecondScreen()));
+              },
+              child: Text("First Button"))
+        ],
+      ),
+    );
+  }
+}
---
+++ navigation/lib/Screens/second_screen.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+
+class SecondScreen extends StatefulWidget {
+  const SecondScreen({super.key});
+
+  @override
+  State<SecondScreen> createState() => _SecondScreenState();
+}
+
+class _SecondScreenState extends State<SecondScreen> {
+  @override
+  Widget build(BuildContext context) {
+    return  Scaffold(
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text("Second Screen"),
+          ElevatedButton(onPressed: (){
+            Navigator.pop(context);
+          }, child: const Text("Button"))
+        ],
+      ),
+    );
+  }
+}
